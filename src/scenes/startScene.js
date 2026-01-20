816ee6442a299eb32868301996a9506f7c5212b0
jhonnierlopez07-code <jhonnierlopez07@gmail.com>
2026-01-20 00:11:29 -0500

    Update startScene.js

--- src/scenes/startScene.js
+++ src/scenes/startScene.js
@@ -1,3 +1,5 @@
+
+
 import { globals } from '../globals.js';
 
 export class StartScene extends Phaser.Scene {
@@ -10,18 +12,71 @@ export class StartScene extends Phaser.Scene {
     }
 
     create() {
+        
         this.add.image(480, 270, 'bg_menu');
-        this.add.text(300, 160, 'ECO-RESCATE', { fontSize: '40px', color: '#2ecc71' });
-        this.add.text(250, 220, 'Recolecta y recicla la basura', { fontSize: '18px' });
 
-        const btn = this.add.text(410, 300, 'EMPEZAR', {
-            fontSize: '22px', backgroundColor: '#27ae60', padding: { x: 16, y: 10 }
+        
+        this.add.text(300, 120, 'ECO-RESCATE', {
+            fontSize: '40px',
+            color: '#2ecc71',
+            fontStyle: 'bold'
+        });
+
+
+        
+        const panel = this.add.graphics();
+
+        
+        panel.fillGradientStyle(
+            0xbaf5c8, 
+            0xbaf5c8,
+            0xeafff0, 
+            0xeafff0,
+            1
+        );
+
+        
+        panel.fillRoundedRect(180, 180, 600, 180, 20);
+
+        
+        panel.lineStyle(2, 0x2ecc71, 0.8);
+        panel.strokeRoundedRect(180, 180, 600, 180, 20);
+
+       
+
+        
+        this.add.text(200, 195, 
+            'Descripción:\nRecolecta basura y deposítala en el contenedor correcto\npara proteger el medio ambiente y asi sumar puntos.',
+            {
+                fontSize: '16px',
+                color: '#145a32',
+                wordWrap: { width: 560 }
+            }
+        );
+
+       
+        this.add.text(200, 255,
+            '¿Cómo jugar?\n• Usa las flechas ← → para moverte\n• Salta con ↑\n• Recoge la basura correcta\n• Tiempo estimado por cada nivel: 90 segundos',
+            {
+                fontSize: '16px',
+                color: '#145a32',
+                wordWrap: { width: 560 }
+            }
+        );
+
+    
+
+        const btn = this.add.text(410, 390, 'EMPEZAR', {
+            fontSize: '22px',
+            backgroundColor: '#27ae60',
+            color: '#ffffff',
+            padding: { x: 20, y: 10 }
         }).setInteractive();
 
+    
         btn.on('pointerdown', () => {
             globals.level = 1;
             globals.score = 0;
             this.scene.start('GameScene');
         });
     }
-}
